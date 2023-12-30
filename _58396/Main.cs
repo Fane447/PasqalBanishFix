c86fde7bf2037fe2afed13ecee226304a18c8d95
Fane O'Callaghan <fane447@gmail.com>
2023-12-30 15:51:10 +0000

    Init

---
+++ _58396/Main.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace _58396;
+
+#if DEBUG
+[EnableReloading]
+#endif
+static class Main
+{
+    internal static Harmony HarmonyInstance;
+    internal static UnityModManager.ModEntry.ModLogger log;
+
+    static bool Load(UnityModManager.ModEntry modEntry)
+    {
+        log = modEntry.Logger;
+#if DEBUG
+        modEntry.OnUnload = OnUnload;
+#endif
+        modEntry.OnGUI = OnGUI;
+        HarmonyInstance = new Harmony(modEntry.Info.Id);
+        HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        return true;
+    }
+
+    static void OnGUI(UnityModManager.ModEntry modEntry)
+    {
+
+    }
+
+#if DEBUG
+    static bool OnUnload(UnityModManager.ModEntry modEntry)
+    {
+        HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+        return true;
+    }
+#endif
+}
---
+++ _58396/Remove_Cue_Blueprint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using HarmonyLib;
+
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.DialogSystem.Blueprints;
+using Kingmaker.ElementsSystem;
+
+
+namespace _58396;
+
+
+
+[HarmonyPatch(typeof(BlueprintsCache))]
+static class Remove_Cue_Blueprint_Patch
+{
+    private static bool loaded = false;
+    [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+    static void Postfix()
+    {
+        if (loaded) return;
+        loaded = true;
+
+        var dialog = ResourcesLibrary.TryGetBlueprint<BlueprintDialog>("c4131a3d9a594c9b99e0d729d5422a71");
+        dialog.FirstCue.Cues.RemoveAt(0);
+
+    }
+}
